c747e576762e43cb96676c003276a0e9c11d78d4
HubertDrazkowski <120560429+HubertDrazkowski@users.noreply.github.com>
2026-03-03 21:41:12 +0000

    Deploying to gh-pages from @ HubertDrazkowski/hubertdrazkowski.github.io@b9a35905171d28867f97ba97346d28b82484f903 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "HubertDrazkowski/hubertdrazkowski.github.io8",
+    "data-repo": "HubertDrazkowski/hubertdrazkowski.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
