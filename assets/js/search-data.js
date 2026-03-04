7e9f72bfcc31ee1b14c595b56193d694a5a8ea78
HubertDrazkowski <120560429+HubertDrazkowski@users.noreply.github.com>
2026-03-04 08:33:41 +0000

    Deploying to gh-pages from @ HubertDrazkowski/hubertdrazkowski.github.io@9753ec118d82a37c264f10dc29e5834986023720 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -484,42 +484,21 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/CV_2025.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("mailto:%71%6C%76%38%37%37@%6B%75.%64%6B", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=Hv2uaWkAAAAJ&hl", "_blank");
         },
       },{
       id: 'light-theme',
